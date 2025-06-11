14d00846bcd732c3e7d36fa26db36de4a3fcc3f7
monishpn <monish.pn@zop.dev>
2025-06-11 16:27:28 +0530

    func commit

--- functions.go
+++ functions.go
@@ -4,11 +4,18 @@ import (
 	"fmt"
 )
 
-func addition(a, b int64) int64 {
-	return a + b
+func addition(a, b int64) (c int64) {
+	c = a + b
+	return
 }
 
 func main() {
-	c := addition(4, 5)
+	var a, b int64
+
+	fmt.Print("Enter the value for A")
+	fmt.Scanf("%d", &a)
+	fmt.Print("Enter the value for B")
+	fmt.Scanln(&b)
+	c := addition(a, b)
 	fmt.Printf("The sum is: %d\n", c)
 }
